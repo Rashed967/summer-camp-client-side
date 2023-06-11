9f534077bd9e9ba75c5c517b9c3f1512397c3d13
Rashed967 <rashedhasan967@gmail.com>
2023-06-12 02:00:26 +0600

    some bug fixed

--- src/pages/InstructorDashboard/MyClassInstructor.jsx
+++ src/pages/InstructorDashboard/MyClassInstructor.jsx
@@ -1,11 +1,95 @@
-import React from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { Button, Space, Table } from 'antd';
+import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const MyClassInstructor = () => {
-    return (
-        <div>
-            <h1>My class instructor</h1>
-        </div>
-    );
+    const {user, loading} = useContext(AuthContext)
+   
+    // const [email, setEmail] = useState('')
+    if(loading){
+    <p>Loading...</p>
+    }
+    
+    const queryClient = useQueryClient();
+    
+    const deleteData = async (id) => {
+        const response = await axios.delete(`http://localhost:5000/bookedClasses/${id}`);
+        console.log(response.data)
+        refetch
+        return response.data;
+    };
+    const mutation = useMutation(deleteData, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('selectedClasses');
+          },
+    });
+    
+      const handleDelete = (id) => {
+        mutation.mutate(id);
+        console.log(id)
+        
+      };
+      
+      const handldePay = id => {
+        console.log(id)
+      }
+
+
+    const {data : selectedClasses = [], isLoading : selectedClassLoading, refetch} = useQuery({
+        queryKey : ['selectedClasses'],
+        queryFn : async () => {
+            const respons = await axios.get(`http://localhost:5000/classessByEmail?email=hossain@mail.com`)
+            
+            if(selectedClassLoading){
+                <p>Loading</p>
+                refetch()
+            }
+            return respons.data
+        }
+        
+    })
+
+    const columns = [
+        {
+          title: 'Name',
+          dataIndex: "name",
+          key: 'name',
+          render: (text) => <a>{text}</a>,
+        },
+        {
+          title: 'Price',
+          dataIndex: 'price',
+          key: 'price',
+        },
+        {
+          title: 'Available Seats',
+          dataIndex: 'availableSeats',
+          key: 'availableSeats',
+        },
+        {
+          title: 'Instructor',
+          key: 'instructor',
+          dataIndex: 'instructor',
+        },
+        {
+          title: 'Action',
+          key: 'action',
+          render: (_, record) => (
+            <Space size="middle">
+              <Button onClick={() => handldePay(record._id)} className='bg-purple-600' type='primary'>Payment</Button>
+              <Button onClick={() =>handleDelete(record._id)} className='bg-danger-700' type='primary'>Delete</Button>
+              
+            </Space>
+          ),
+        },
+      ];
+    
+
+
+
+    return  <Table columns={columns} dataSource={selectedClasses} />
 };
 
 export default MyClassInstructor;
